17b38d58d9a51cfeb00a69410d8414ed5e106bc8
Anjali Rawat <158160439+anjalirawat-er@users.noreply.github.com>
2026-07-06 13:10:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Second Highest Salary/second-highest-salary.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+class Solution:
+    def secondHighestSalary(self, employee: pd.DataFrame) -> pd.DataFrame:
+        distinct_salaries = employee['salary'].drop_duplicates()
+        sorted_salaries = distinct_salaries.sort_values(ascending=False)
+        if len(sorted_salaries) >= 2:
+            second_highest = sorted_salaries.iloc[1]
+        else:
+            second_highest = None
+            
+        return pd.DataFrame({'SecondHighestSalary': [second_highest]})
