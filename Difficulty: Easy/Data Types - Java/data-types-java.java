32899d0fa9eddfee9e5a65ac4fc75870e1eb3e02
Anjali Rawat <158160439+anjalirawat-er@users.noreply.github.com>
2026-07-17 11:54:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Data Types - Java/data-types-java.java
@@ -0,0 +1,14 @@
+class Geeks {
+
+    // Function to do operations with different data types
+    static void dataTypes(int a, float b, double c, long l, byte d) {
+
+        double p = c/b; // c/b
+        double q = b/a; // b/a
+        double r = c/a; // c/a
+        double m = r+l; // r+l
+        int s = a/d ;    // a/d
+
+        System.out.println(p + " " + q + " " + r + " " + m + " " + s);
+    }
+}
