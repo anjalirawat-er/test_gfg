bb065254d7e2371296bdcd206ec6c9b0563114e1
Anjali Rawat <158160439+anjalirawat-er@users.noreply.github.com>
2026-07-20 14:44:55 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/TypeCast And Double It/type-cast-and-double-it.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        String num = sc.nextLine();
+        int n = Integer.parseInt(num);
+        int value=n*2;
+        System.out.println(value);
+    }
+}
