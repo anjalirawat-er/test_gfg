2ddaf583dfce7fffb0a4e05d291fd7a95c63ebfb
Anjali Rawat <158160439+anjalirawat-er@users.noreply.github.com>
2026-07-31 12:49:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Palindrome Digit Sum/palindrome-digit-sum.java
@@ -0,0 +1,19 @@
+class Solution {
+    boolean isDigitSumPalindrome(int n) {
+        // code here
+        int sum=0;
+        while(n>0){
+            sum +=n % 10;
+            n/=10;
+        }
+        
+        int original=sum;
+        int rev=0;
+        while (sum>0){
+            rev = rev*10 + sum%10;
+            sum/=10;
+        }
+        
+        return original==rev;
+    }
+}
