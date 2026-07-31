7e6f523bd070e91dd17b77d8a19934223e144ab0
Anjali Rawat <158160439+anjalirawat-er@users.noreply.github.com>
2026-07-31 12:36:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Median of an Array/median-of-an-array.java
@@ -0,0 +1,12 @@
+class Solution {
+    public double findMedian(int[] arr) {
+        // Code here.
+        Arrays.sort(arr);
+        int n = arr.length;
+        if (n % 2 != 0) {
+            return (double) arr[n / 2];
+        } else {
+            return (arr[n / 2] + arr[(n / 2) - 1]) / 2.0;
+        }
+    }
+}
