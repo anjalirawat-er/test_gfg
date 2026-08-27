bdf540db67a3ffd8f3f6a1aa8339430d7df35ba4
Anjali Rawat <158160439+anjalirawat-er@users.noreply.github.com>
2026-08-27 17:48:47 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Subset Sums/subset-sums.java
@@ -0,0 +1,17 @@
+class Solution {
+    public ArrayList<Integer> subsetSums(int[] arr) {
+        ArrayList<Integer>ans=new ArrayList<>();
+        int subset=(int)Math.pow(2,arr.length);
+
+        for(int i=0;i<subset;i++){
+              int sum=0;
+              for(int j=0;j<arr.length;j++){
+
+                    if((i&(1<<j))!=0)sum+=arr[j];
+              }
+              ans.add(sum);
+        }
+
+        return ans;
+    }
+}        
